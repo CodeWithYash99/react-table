797d8e17698cde537f4b8fd5646d6da82b584d52
codewithyash <yashwanthrangu99@gmail.com>
2024-07-12 16:54:59 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+import "./App.css";
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  // const fetchdata = async () => {
+  //   const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  //   const data = await res.json();
+  //   setData(data);
+  //   console.log(data);
+  // };
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(data => setData(data))
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container d-flex flex-column align-items-center">
+      <table>
+        <thead>
+          <tr>
+            <th className="th">ID</th>
+            <th className="th">Name</th>
+            <th className="th">Username</th>
+            <th className="th">Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.map((each) => (
+            <tr key={each.id}>
+              <td className="td">{each.id}</td>
+              <td className="td">{each.name}</td>
+              <td className="td">{each.username}</td>
+              <td className="td">{each.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default App;
